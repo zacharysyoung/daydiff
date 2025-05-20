ba9263d72a8744c4ca3a70a7ddd06f814246d527
Zach Young <zacharysyoung@gmail.com>
2025-05-20 13:04:56 -0700

    added more date layouts, and versatility

--- main.go
+++ main.go
@@ -1,31 +1,42 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"log"
 	"os"
 	"time"
 )
 
 func main() {
 	if len(os.Args) != 3 {
-		fmt.Fprintln(os.Stderr, "usage: daydiff date1 date2")
+		fmt.Fprintf(os.Stderr, `usage: daydiff date1 date2
+
+Acceptable date formats:
+  - January 2 2006
+  - Jan 2 2006
+  - 1/2/2006
+  - 2006/01/02
+  - 2006-01-02
+
+and any of the above without the year.
+`)
 		os.Exit(2)
 	}
 	sa, sb := os.Args[1], os.Args[2]
 
 	a, err := toTime(sa)
 	if err != nil {
-		fatalf("bad time: %s:", sa)
+		fatalf("bad time %s: %v", sa, err)
 	}
 	b, err := toTime(sb)
 	if err != nil {
-		fatalf("bad time: %s:", sb)
+		fatalf("bad time %s: %v", sb, err)
 	}
 
 	if a.Before(b) {
 		a, b = b, a
 	}
+
 	i := 0
 	for ; b.Before(a); i++ {
 		b = b.Add(24 * time.Hour)
@@ -39,28 +50,44 @@ func toTime(s string) (time.Time, error) {
 		t   time.Time
 		err error
 
-		year = fmt.Sprintf("%d", time.Now().Year())
+		parse = func(s string) error {
+			for _, layout := range []string{
+				"2006-01-02",
+				"2006/01/02",
+				"1/2/2006",
+				"Jan 2 2006",
+				"January 2 2006",
+			} {
+				t, err = time.Parse(layout, s)
+				if err == nil {
+					log.Printf("parsed %s as %s", s, layout)
+					return nil
+				}
+				log.Printf("could not parse %s: %v", s, err)
+			}
+			return fmt.Errorf("could not parse %s", s)
+		}
 	)
 
-	t, err = time.Parse("2006/01/02", s)
-	if err == nil {
-		return t, nil
-	}
-	t, err = time.Parse("2006/01/02", year+"/"+s)
-	if err == nil {
+	// try date s as-is
+	if err = parse(s); err == nil {
 		return t, nil
 	}
 
-	t, err = time.Parse("1/2/2006", s)
-	if err == nil {
-		return t, nil
-	}
-	t, err = time.Parse("1/2/2006", s+"/"+year)
-	if err == nil {
-		return t, nil
+	// that didn't work, try adding this year
+	year := fmt.Sprintf("%d", time.Now().Year())
+
+	for _, ss := range []string{
+		year + "/" + s,
+		year + "-" + s,
+		s + " " + year,
+	} {
+		if err = parse(ss); err == nil {
+			return t, nil
+		}
 	}
 
-	return t, errors.New("could not parse")
+	return t, err
 }
 
 func fatalf(format string, args ...any) {
